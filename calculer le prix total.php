57550ec8fb0513da94a16d7de849269790419524
NAchref <64566472+NAchref@users.noreply.github.com>
2022-03-17 14:14:17 +0100

    Create calculer le prix total.php

---
+++ calculer le prix total.php
@@ -0,0 +1,21 @@
+<?php
+
+$discount= 10;
+$price= array(7,13.5,3,8,2,1);
+calculateTotalPrice( $price , $discount);
+
+function calculateTotalPrice(array $price , $discount){
+   
+ $sum = 0;
+ rsort($price);
+ $rem = $discount/100 ;
+ for($i=0; $i<count($price); $i++ )
+     {
+        $sum = $sum + $price[$i];
+        $total= $sum - $rem*$price[0];
+     }
+      return floor($total);
+}
+
+
+?>
