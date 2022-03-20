737ac85c3edb76c4927b3a5e51cffedd3abe0abe
NAchref <64566472+NAchref@users.noreply.github.com>
2022-03-20 15:56:29 +0100

    Update tableunique.php

--- tableunique.php
+++ tableunique.php
@@ -1,1 +1,29 @@
 <?php
+
+$tab= array(7,3,6,3,4,6,9);
+filterDuplicates($tab);
+
+function filterDuplicates(array $data)
+{
+  filter= [];
+  $i= 0;
+  foreach($data as $value)
+  {
+    $add = TRUE;
+    foreach($filter as $valeur)
+    {
+      if ($value == $valeur)
+      {
+        $add = FALSE;
+      }       
+    }
+    if($add)
+    {
+      filter[$i]= $value;
+      i++;
+    }
+  }
+  return $filter;
+}
+
+?>
